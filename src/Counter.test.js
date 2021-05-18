02ff88cac3ebd4950c6e5d17f72bff7ad071173b
Felix Anducho <felix.anducho74@gmail.com>
2021-05-17 20:26:58 -0500

    Added tests for the increment and decrement buttons. Added Cypress and MSW for End-2-end testing

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { render, screen } from '@testing-library/react'
+import { prettyDOM, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Counter from './Counter'
 
 test('Counter renders', () => {
@@ -9,3 +10,20 @@ test('Counter renders', () => {
     screen.getByRole('button', { name: /decrease/i })
 })
 
+test('User clicks increase and the counter count is 1', () => {
+    render(<Counter/>)
+    const message = screen.getByText('Current count: ', { exact: false })
+    const increment = screen.getByRole('button', { name: /increase/i }) 
+    expect(message).toHaveTextContent('Current count: 0')
+    userEvent.click(increment)
+    expect(message).toHaveTextContent('Current count: 1')
+})
+
+test('User clicks decrease and the counter count is -1', () => {
+    render(<Counter/>)
+    const message = screen.getByText('Current count: ', { exact: false })
+    const decrease = screen.getByRole('button', { name: /decrease/i }) 
+    expect(message).toHaveTextContent('Current count: 0')
+    userEvent.click(decrease)
+    expect(message).toHaveTextContent('Current count: -1')
+})
